5fd23afb4f67dc0a4d25ed87434e0462d268340e
Weerapat1455 <60087363+Weerapat1455@users.noreply.github.com>
2020-01-24 21:47:46 +0700

    Q1

---
+++ Q1.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  DDRD=0xFC;
+  DDRB=0x03;
+  DDRC=0xFF;
+}
+
+void loop()
+{
+  int i=0;
+  PORTD=0x01;
+  if()
+  {
+    PORTC=0x01;  //0b0000 0001
+  }
+  else if()
+  {
+    PORTC=0x02;
+  }
+    
+  PORTD=0x01<<i;
+
+}
