08c86c96c67b28a88cd3afe46d53ebd00a5db67b
Weerapat1455 <60087363+Weerapat1455@users.noreply.github.com>
2020-05-26 00:20:05 +0700

    Add files via upload

---
+++ Week8/1.ino
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+
+#define LED_PIN_1 11
+#define LED_PIN_2 3
+
+float brightness = 0;
+int pin_led, pin_on;
+String buff;
+
+void setup()
+{ 
+  Serial.begin(9600);
+  pinMode(LED_PIN_1, OUTPUT);
+  pinMode(LED_PIN_2, OUTPUT);
+}
+
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    char inByte = Serial.read();
+    if (inByte == 'L'){
+      buff = "";
+    }
+    else if (('0' <= inByte) && (inByte <= '9')){
+      buff += inByte;
+    }
+    else if (inByte == 'B'){
+      if(buff.toInt() == 1) { pin_on = 11; }
+      if(buff.toInt() == 2) { pin_on = 5; }
+      buff = "";
+    }
+    else if (inByte == 'N'){
+      brightness = buff.toInt();
+      Serial.println(brightness);
+      analogWrite(pin_on, (int(brightness)) * 2.54);
+    }
+  }
+}
